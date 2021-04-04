fe3462bf918922470b3244b2e01e86e931d98d36
sabesan1998 <sabesanganeshalingam@gmail.com>
2021-04-04 21:08:13 +0530

    final

--- src/Components/orders.jsx
+++ src/Components/orders.jsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component}  from 'react';
+import Modal from 'react-modal';
 //import NodeFetch from 'node-fetch';
 import OrderTable from './table';
 class Orders extends Component {
     state = { 
         orders:[],
         branch:'',
-        service:''
+        service:'',
+        checkedList:[],
+        modelState:false
 
         
      }
@@ -22,40 +25,96 @@ class Orders extends Component {
         } 
         handleBranch = (e)=>{
             this.setState({branch: e.target.value});
+             
         }  
         handleService =(e)=>{
             this.setState({service: e.target.value});
 
         }
+        addList=(orderItem)=>{
+          this.setState({checkedList:[...this.state.checkedList,orderItem]})
+        }
+        removeList=(orderItem) => {
+            this.setState({checkedList:this.state.checkedList.filter(order=>{
+                return order !== orderItem
+            } ) })
+        }
+        changeState = () =>{
+             if(this.state.modelState == false){
+                 this.setState({modelState: true})
+             }
+             else{
+                this.setState({modelState: false})
+             }
+        }
+        changeToComplete = () =>{
+            return this.state.orders.status == 'Completed'
+        }
 
     
     render() { 
-        return(
-            <div>
-              
-              <div className="tb">
-     <h2>Orders</h2>
-     <div>
-     <span><label>Filter by Branch</label><br></br>
-        <select value={this.state.branch} onChange={this.handleBranch}>
-        <option  value="" selected>ALL</option>
-        
-        {this.state.orders.map(order =>
-            <option  value={order.branch} selected>{order.branch}</option>
-            )}
-        </select><br></br>
-        <br></br>
-        <label>Filter by Service</label><br></br>
-        <select value={this.state.service} onChange={this.handleService}><option  value="" selected>ALL</option>
-        
-        {this.state.orders.map(order =>
-            <option  value= {order.service} selected>{order.service}</option>
-            )}</select></span>
-    </div>
-    <br></br>
+        let uniqueBranch= [...new Set(this.state.orders.map(order =>order.branch))];
+        let uniqueService= [...new Set(this.state.orders.map(order =>order.service))];
+
+    return(
     <div>
-        <button>Mark as complete</button>
-    </div>
+        <div className="tb">
+           <h2>Orders</h2>
+                 <div style={{display:'block'}}>
+         
+                <label>Filter by Branch</label> <span style={{marginLeft:10}}></span>
+                <select style={{width:150}} value={this.state.branch} onChange={this.handleBranch}>
+                <option  value="" selected>ALL</option>
+        
+                {uniqueBranch.map(un => 
+                   <option  value={un} selected>{un}</option>
+                   )}
+                </select><span style={{marginLeft:30}}></span>
+       
+     
+            <label > Filter by Service</label><span style={{marginLeft:10}}></span>
+            <select style={{width:150 }} value={this.state.service} onChange={this.handleService}>
+            <option  value="" selected>ALL</option>
+        
+            {uniqueService.map(us =>
+            <option  value= {us} selected>{us}</option>
+            )}</select> 
+             
+            <button 
+            style={{marginLeft:500,background:'black',color:'white'}} onClick={() => this.changeState()}>
+            <span style={{backgroundColor:'gray'}}>{this.state.checkedList.length}</span>
+                 Mark as Complete
+                 </button>
+
+                <Modal isOpen={this.state.modelState} style={{width:20}}>
+                <h4>Mark as complete</h4>
+                <p>Are you sure to mark these orders as complete?</p>
+                {this.state.checkedList.map(mark =>{
+                
+                return(
+                <div style={{border:'1px solid black',margin:'20px 0px'}}>
+                 <p><span style={{marginLeft:80}}></span>{mark.id}
+                 <span style={{marginLeft:400}}></span>{mark.service}
+                 <span style={{marginLeft:400}}></span>{mark.customer.name}
+                 <br></br><span style={{marginLeft:80}}></span>{mark.branch}
+                 <span style={{marginLeft:850}}></span>{mark.customer.city}
+                 </p>
+         </div>
+                )})}
+                 
+        <button style={{marginLeft:850}} onClick={() => this.changeState()}>
+             Cancel
+         </button>
+        <button style={{marginLeft:50}} onClick={() => this.changeToComplete}  >
+            Marks as Complete
+            
+         </button>
+
+         </Modal> 
+                 
+        </div>
+        <br></br>
+     
     <table className={'w3-table w3-bordered'} border={'1px'}>
          <tr>
              <th></th>
@@ -69,13 +128,24 @@ class Orders extends Component {
          </tr>
          
 
-         {this.state.orders.map(order =><OrderTable order={order} branch={this.state.branch} />)}
+         {this.state.orders.map(order =><OrderTable 
+         order={order} 
+         branch={this.state.branch} 
+         service={this.state.service}
+         addList={this.addList}
+         removeList={this.removeList}
+
+          />)}
+           
          
     </table>
 </div>
+  
+ 
+</div> 
 
-
-            </div>
+ 
+           
         );
     }
 }
--- src/Components/table.jsx
+++ src/Components/table.jsx
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
 class OrderTable extends Component {
-    state = { order:this.props.order }
+    state = { 
+        order:this.props.order, 
+        checked:true
+        
+    }
+    handleCheckBox = (e)=>{
+        
+       if(this.state.checked){
+            
+           this.setState({checked: false})
+           this.props.addList(this.state.order)
+
+       }
+       else{
+        this.setState({checked: true}) 
+        this.props.removeList(this.state.order)
+
+        
+       }
+         
+
+    }
 
     display=()=>{
-        if (this.props.branch === this.state.branch || this.props.branch === '')
+        if ((this.props.branch  == this.state.order.branch  || this.props.branch == '') && (this.props.service  == this.state.order.service  || this.props.service == ''))
         {
-            return <tr>
-            <td><input value={this.state.order.id} type={"checkbox"}/></td>
+             
+            return(
+        
+        <tr className={this.state.order.status == 'Completed' ? 'changeColor' : ''}>
+            <td><input   type={"checkbox"}  onChange={this.handleCheckBox}/></td>
 
            <td>{this.state.order.id}</td>
             
-           <td>{this.state.order.customer.name}<br></br>{this.state.order.customer.city}
-           </td>
-           <td>{this.state.order.addedby}</td>
+           <td>{this.state.order.customer.name}<br></br>{this.state.order.customer.city}</td>
+           <td> {this.state.order.addedby}</td>
            <td>{this.state.order.reference}</td>
-           <td>{this.state.order.branch}</td>
-           <td>{this.state.order.service}</td>
-           <td>{this.state.order.status}</td>
+           <td> 
+           {this.state.order.branch}</td>
+           <td>  
+           {this.state.order.service}</td>
 
-           </tr>
-        }
+         
+          <td>{this.state.order.status}</td>
+          </tr>
+            );
+                
+           
+           }
+           }
 
-    }
-    render() { 
+           
+
+          
+        
+
+    
+    render(){ 
         return(
            <React.Fragment>{this.display()}</React.Fragment>
 
